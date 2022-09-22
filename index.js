e6f2bcbd3cc1f3cdc25bb87266ed30056dbb7d30
hasankoroglu <nurcihanunsur@gmail.com>
2022-09-22 21:09:13 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const btnEl = document.querySelector(".btn");
+btnEl.addEventListener("mouseover", event => {
+    const x = event.pageX - btnEl.offsetLeft;
+    const y = event.PageY - btnEl.offsetTop;
+
+    btnEl.style.setProperty("--xPos", x + "px");
+    btnEl.style.setProperty("--yPos", y + "px");
+});
